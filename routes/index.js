3b5b6d431881c9afaba0721ef197ba752a8055ad
Hero <sihao387@gmail.com>
2017-10-27 00:37:50 -0400

    Removed universal analytics as it is currently not needed.

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,25 @@
 // Routing page for pages on the root level
 
 var express = require('express');
-var ua = require('universal-analytics');
-var visitor = ua('UA-47080716-6', {https: true});
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    visitor.pageview('/').send();
     res.render('index', {title: 'Index'});
 });
 
 router.get('/timer', function (req, res) {
-    visitor.pageview('/timer').send();
     res.render('index', {title: 'Timers'});
 });
 
 router.get('/map', function (req, res) {
-    visitor.pageview('/map').send();
     res.render('map', {title: 'Map'});
 });
 
 router.get('/fish', function (req, res) {
-    visitor.pageview('/map').send();
     res.render('fish', {title: 'Fish'});
 });
 
 router.get('/howtofish', function (req, res) {
-    visitor.pageview('/howtofish').send();
     res.render('howtofish', {title: 'How to Fish'});
 });
 

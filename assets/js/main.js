a78742d15a8b88d599489c3a6a15f5d37b06c142
NeXoGone <nexogone@gmail.com>
2018-01-24 03:40:03 +1000

    Loader (SVG)

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,26 @@ function getObjects(obj, key, val) {
   return objects;
 }
 
+//Loader function
+function updateSVG() {
+  $('.loader').each(function() {
+    var $img = jQuery(this);
+    var imgURL = $img.attr('src');
+    var attributes = $img.prop("attributes");
+
+    $.get(imgURL, function(data) {
+      var $svg = jQuery(data).find('svg');
+      $svg = $svg.removeAttr('xmlns:a');
+      $.each(attributes, function() {
+        $svg.attr(this.name, this.value);
+      });
+      $img.replaceWith($svg);
+    }, 'xml');
+  });
+}
+
+updateSVG();
+
 // Update data that is being used by this page
 function updateDataDependencies() {
   cetusIsDay = worldState.cetusCycle.isDay;

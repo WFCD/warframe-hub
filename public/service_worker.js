d89be199cee1627728c35bcfbd1c70a4193a1cfd
Tobiah <tobiah@warframe.gg>
2018-08-14 13:11:15 -0500

    feat: more caching of static content (#164)
    * feat: more caching of static content
    
    * add map and map images to cache
    
    * move bootstrap night mode loading to preload
    
    * move css to dependencies, preloaded in header
    
    * don't preload all of the map images, just the base page
    
    * move bootstrap nightmode back to critical
    
    * move bootstrap night back to criticalassets

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,21 +1,32 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                'js/main.js',
-                'js/updaters.js'
-            ]);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        'timer',
+        'js/main.js',
+        'js/updaters.js',
+        'css/bootstrap-default.min.css',
+        'css/bootstrap-night.min.css',
+        'css/common.css',
+        'css/main.css',
+        'css/main.eidolon.css',
+        'css/main.night.css',
+        'css/main.retro.css',
+        'img/loading.svg',
+        'map',
+        'fish',
+        'howtofish',
+        '404'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(
-            function (response) {
-                return response || fetch(e.request);
-            }
-        )
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });

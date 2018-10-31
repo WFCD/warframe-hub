ba7ce342a997e3ebabcd9397d4bb48145d01280e
Bitti09 <bitti06@gmail.com>
2018-10-31 21:35:59 +0100

    Add News Panel

---
+++ src/components/panels/NewsPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-col md="6">
+        <h3 class="text-center">{{headertext}}</h3>
+        <b-carousel id="infoscreen-carousel" :interval="9000" :indicators="false" :controls="true">
+          <b-carousel-slide   v-for="(newsitem,index) in news" :key="index"
+          v-if="newsitem.translations['en']"
+          :text="newsitem.translations['en']">
+          <img slot="img" class="d-block w-100" width="1024" height="250"
+          :src="newsitem.imageLink" alt="image slot">
+          <p>
+            Posted: {{newsitem.eta}} | More Info: <a :href="newsitem.link" target="_blank">Click</a>
+          </p>
+        </b-carousel-slide>
+      </b-carousel>
+    </b-col>
+</template>
+<style>
+.carousel-caption {
+  background-color: rgba(64, 64, 64, 0.7) !important;
+  color: white;
+  width: 100% !important;
+  left: 0;
+  bottom: 0;
+  font-weight: bolder;
+  padding-bottom: 0;
+}
+.carousel-caption a {
+  color: white;
+}
+</style>
+<script>
+export default {
+  props: ['news'],
+  name: 'NewsPanel',
+  computed: {
+    headertext() {
+      return 'News';
+    }
+  },
+  data() {
+    return {
+      styleObject: {
+        display: 'inline'
+      }
+    };
+  }
+};
+</script>
--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -2,17 +2,20 @@
   <div class="timers">
     <b-container fluid>
       <AlertPanel v-if="this.$store.getters.componentState.alerts.state"/>
+      <NewsPanel  v-if="this.$store.getters.componentState.news.state" :news="this.$store.getters.worldstate.news" />
     </b-container>
   </div>
 </template>
 
 <script>
 import AlertPanel from '@/components/panels/AlertPanel.vue';
+import NewsPanel from '@/components/panels/NewsPanel.vue';
 
 export default {
   name: 'timers',
   components: {
-    AlertPanel
+    AlertPanel,
+    NewsPanel
   }
 };
 </script>

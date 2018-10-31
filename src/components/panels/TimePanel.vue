0f5b85a1693e16aa7ece9320a1a76e4b573eb564
Bitti09 <bitti06@gmail.com>
2018-10-31 21:38:36 +0100

    Add Time Panel for Cetus & Earth + Simple Grid Layout

---
+++ src/components/panels/TimePanel.vue
@@ -0,0 +1,56 @@
+<template>
+<b-col md="6">
+  <h3 class="text-center">{{headertext}}</h3>
+  <b-list-group>
+    <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+      <span class="pull-left">
+        <b>Currently it is:</b>
+      </span>
+      <b-badge v-if="time.isDay" class="pull-right timebadage" variant="success">Day</b-badge>
+      <b-badge v-else class="pull-right timebadage" variant="dark">Night</b-badge>
+    </b-list-group-item>
+    <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+      <span class="pull-left">
+        <b>Time until <span v-if="!time.isDay">Day</span><span v-else>Night</span>: </b>
+      </span>
+      <b-badge variant="info" :style="styleObject" class="pull-right timebadage">{{time.timeLeft}}</b-badge>
+    </b-list-group-item>
+    <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+      <span class="pull-left">
+        <b>Time at <span v-if="!time.isDay">Day</span><span v-else>Night</span>:</b>
+      </span>
+      <b-badge variant="info" :style="styleObject" class="pull-right timebadage" >{{earthtimezonetime}}</b-badge>
+    </b-list-group-item>
+    <b-list-group-item class="list-group-item-borderbottom">
+    </b-list-group-item>
+  </b-list-group>
+</b-col>
+</template>
+<style scoped>
+.timebadage {
+  margin-right: 5px;
+}
+</style>
+<script>
+import moment from 'moment';
+
+export default {
+  props: ['time', 'location'],
+  name: 'TimePanel',
+  computed: {
+    earthtimezonetime() {
+      return moment(this.$props.time.expiryTime).format('llll');
+    },
+    headertext() {
+      return this.$props.location + ' Day/Night Cycle';
+    }
+  },
+  data() {
+    return {
+      styleObject: {
+        display: 'inline'
+      }
+    };
+  }
+};
+</script>
--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="timers">
     <b-container fluid>
-      <AlertPanel v-if="this.$store.getters.componentState.alerts.state"/>
-      <NewsPanel  v-if="this.$store.getters.componentState.news.state" :news="this.$store.getters.worldstate.news" />
+      <b-row>
+        <AlertPanel v-if="this.$store.getters.componentState.alerts.state"/>
+        <TimePanel  v-if="this.$store.getters.componentState.earth.state" :time="this.$store.getters.worldstate.earthCycle"  :location="'Earth'" />
+        <TimePanel  v-if="this.$store.getters.componentState.cetus.state" :time="this.$store.getters.worldstate.cetusCycle"  :location="'Cetus'" />
+        <NewsPanel  v-if="this.$store.getters.componentState.news.state" :news="this.$store.getters.worldstate.news" />
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -10,12 +14,14 @@
 <script>
 import AlertPanel from '@/components/panels/AlertPanel.vue';
 import NewsPanel from '@/components/panels/NewsPanel.vue';
+import TimePanel from '@/components/panels/TimePanel.vue';
 
 export default {
   name: 'timers',
   components: {
     AlertPanel,
-    NewsPanel
+    NewsPanel,
+    TimePanel
   }
 };
 </script>

c36a00cc58216380449f3805f7271a3b9b4d1b69
Bitti09 <bitti06@gmail.com>
2018-10-31 21:40:28 +0100

    Add Daily Reward Reset Panel

--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -6,6 +6,7 @@
         <TimePanel  v-if="this.$store.getters.componentState.earth.state" :time="this.$store.getters.worldstate.earthCycle"  :location="'Earth'" />
         <TimePanel  v-if="this.$store.getters.componentState.cetus.state" :time="this.$store.getters.worldstate.cetusCycle"  :location="'Cetus'" />
         <NewsPanel  v-if="this.$store.getters.componentState.news.state" :news="this.$store.getters.worldstate.news" />
+        <ResetPanel />
       </b-row>
     </b-container>
   </div>
@@ -15,13 +16,15 @@
 import AlertPanel from '@/components/panels/AlertPanel.vue';
 import NewsPanel from '@/components/panels/NewsPanel.vue';
 import TimePanel from '@/components/panels/TimePanel.vue';
+import ResetPanel from '@/components/panels/ResetPanel.vue';
 
 export default {
   name: 'timers',
   components: {
     AlertPanel,
     NewsPanel,
-    TimePanel
+    TimePanel,
+    ResetPanel
   }
 };
 </script>

94ca936b68e711d1a26f12f30fe451a7bf530cfd
Bitti <Bitti09@users.noreply.github.com>
2018-11-19 21:44:27 +0100

    Add News , DailyReset & TimerPanel + add Vallis to store Components (#209)
    * Add News , DailyReset & TimerPanel + add Vallis to store Components
    
    * Fix row display

---
+++ src/components/panels/NewsPanel.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-col md="6">
+        <h3 class="text-center">{{headertext}}</h3>
+        <b-carousel id="infoscreen-carousel" :interval="9000" :indicators="false" :controls="true">
+          <b-carousel-slide   v-for="(newsitem,index) in news" :key="index"
+          v-if="newsitem.translations['en']"
+          :text="newsitem.translations['en']">
+          <b-img slot="img" class="d-block slide-image" fluid center 
+          :src="newsitem.imageLink" alt="image slot"/>
+            <p>
+            Posted: {{newsitem.eta}} | More Info: <a :href="newsitem.link" target="_blank">Click</a>
+            </p>
+        </b-carousel-slide>
+      </b-carousel>
+    </b-col>
+</template>
+<style>
+.carousel-caption {
+  background-color: rgba(64, 64, 64, 0.7) !important;
+  color: white;
+  width: 100% !important;
+  left: 0 !important;
+  bottom: 0 !important;
+  font-weight: bolder;
+  padding-bottom: 0px !important
+}
+.carousel-caption a {
+  color: white;
+}
+.slide-image {
+  max-height: 250px !important;
+  min-height: 230px !important;
+}
+</style>
+<script>
+export default {
+  props: ['news'],
+  name: 'NewsPanel',
+  computed: {
+    headertext() {
+      return 'News';
+    }
+  },
+  data() {
+    return {
+      styleObject: {
+        display: 'inline'
+      }
+    };
+  }
+};
+</script>
---
+++ src/components/panels/TimePanel.vue
@@ -0,0 +1,106 @@
+<template>
+  <b-col md="6">
+    <h3 class="text-center">{{headertext}}</h3>
+    <b-list-group>
+      <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+        <span class="pull-left">
+          <b>Currently it is:</b>
+        </span>
+        <span v-if="time.isDay && location !=='Vallis'" class="pull-right day">Day</span>
+        <span v-else-if="location !=='Vallis' && !time.isDay" class="pull-right night">Night</span>
+        <span v-else-if="location =='Vallis' && !time.isWarm" class="pull-right cold">Cold</span>
+        <span v-else class="pull-right day">Warm</span>
+      </b-list-group-item>
+      <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+        <span class="pull-left" v-if="location !=='Vallis'">
+          <b>
+            Time until
+            <span v-if="!time.isDay">Day</span>
+            <span v-else>Night</span>:
+          </b>
+        </span>
+        <span class="pull-left" v-else>
+          <b>
+            Time until
+            <span v-if="!time.isWarm">Cold</span>
+            <span v-else>Warm</span>:
+          </b>
+        </span>
+        <TimeBadge :starttime="now" :endtime="time.expiry" :interval="1000"/>
+      </b-list-group-item>
+      <b-list-group-item :style="styleObject" class="list-group-item-borderless">
+        <span class="pull-left" v-if="location !=='Vallis'">
+          <b>
+            Time at
+            <span v-if="!time.isDay">Day</span>
+            <span v-else>Night</span>:
+          </b>
+        </span>
+        <span class="pull-left" v-else>
+          <b>
+            Time at
+            <span v-if="!time.isWarm">Cold</span>
+            <span v-else>Warm</span>:
+          </b>
+        </span>
+        <b-badge
+          variant="info"
+          :style="styleObject"
+          class="pull-right timebadage"
+        >{{earthtimezonetime}}</b-badge>
+      </b-list-group-item>
+      <b-list-group-item class="list-group-item-borderbottom"></b-list-group-item>
+    </b-list-group>
+  </b-col>
+</template>
+<style scoped>
+.timebadage {
+  margin-right: 5px;
+}
+.night:before {
+  color: rgba(51, 211, 255, 0.8);
+  content: "🌙 ";
+}
+.cold:before {
+  color: rgba(51, 211, 255, 0.8);
+  content: "\2744 ";
+}
+.day:before {
+  color: rgba(249, 168, 6, 0.8);
+  content: "☀ ";
+}
+</style>
+<script>
+import TimeBadge from '@/components/TimeBadge.vue';
+import moment from "moment";
+
+export default {
+  props: ["time", "location"],
+  name: "TimePanel",
+  computed: {
+    earthtimezonetime() {
+      return moment(this.$props.time.expiry).format("llll");
+    },
+    now() {
+      return moment().toISOString();
+    },
+    headertext() {
+      if (this.$props.location == "Vallis") {
+        return this.$props.location + " Cold/Warm Cycle";
+      } else {
+        return this.$props.location + " Day/Night Cycle";
+      }
+    }
+  },
+  components: {
+    TimeBadge
+  },
+  data() {
+    return {
+      styleObject: {
+        display: "inline"
+      }
+    };
+  }
+};
+</script>

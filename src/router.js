7e99c8984d55f771e9b0c7c3e80c9deaca72afd6
Tobiah <tobiah@protonmail.com>
2018-11-29 10:02:09 -0600

    add meta descriptors for router

--- src/router.js
+++ src/router.js
@@ -20,36 +20,127 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Timer,
+      meta: {
+        title: 'Warframe Hub',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Warframe hub strives to be the community center of the Warframe game.'
+          },
+          {
+            name: 'og:description',
+            content: 'Warframe hub strives to be the community center of the Warframe game.'
+          },
+        ],
+      },
     },
     {
       path: '/timer',
       name: 'timer',
       component: Timer,
+      meta: {
+        title: 'Warframe Hub',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Warframe hub strives to be the community center of the Warframe game.'
+          },
+          {
+            name: 'og:description',
+            content: 'Warframe hub strives to be the community center of the Warframe game.'
+          },
+        ],
+      },
     },
     {
       path: '/poe/map',
-      name: 'Plains of Exile Map',
+      name: 'Plains of Eidolon Map',
       component: PoEMap,
+      meta: {
+        title: 'Warframe Hub | PoE Map',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Overworld map for the Plains of Eidolon'
+          },
+          {
+            name: 'og:description',
+            content: 'Overworld map for the Plains of Eidolon'
+          },
+        ],
+      },
     },
     {
       path: '/poe/fish',
-      name: 'fish',
+      name: 'Plains of Eidolon Fish',
       component: PoEFish,
+      meta: {
+        title: 'Warframe Hub | PoE Fish',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Fish Information and resources for the Plains of Eidolon'
+          },
+          {
+            name: 'og:description',
+            content: 'Fish Information and resources for the Plains of Eidolon'
+          },
+        ],
+      },
     },
     {
       path: '/poe/fish/howto',
       name: 'howtofish',
       component: HowToFish,
+      meta: {
+        title: 'Warframe Hub | How to Fish',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Guide on how to fish on the Plains of Eidolon'
+          },
+          {
+            name: 'og:description',
+            content: 'Guide on how to fish on the Plains of Eidolon'
+          },
+        ],
+      },
     },
     {
       path: '/vallis/fish',
       name: 'Vallis Fish',
       component: VallisFish,
+      meta: {
+        title: 'Warframe Hub | Orb Vallis Fish',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Fish Information and resources for the Ob Vallis'
+          },
+          {
+            name: 'og:description',
+            content: 'Fish Information and resources for the Ob Vallis'
+          },
+        ],
+      },
     },
     {
       path: '/vallis/map',
       name: 'Orb Vallis Map',
       component: VallisMap,
+      meta: {
+        title: 'Warframe Hub | Orb Vallis Map',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Overworld map for the Orb Vallis'
+          },
+          {
+            name: 'og:description',
+            content: 'Overworld map for the Orb Vallis'
+          },
+        ],
+      },
     },
     {
       path: '/404',
@@ -59,6 +150,6 @@ export default new Router({
     {
       path: '*',
       redirect: '/404'
-    }
-  ]
+    },
+  ],
 });

457a9997fd96f47047adadf98d4e07322b92b477
Tobiah <tobiah@warframe.gg>
2019-01-01 11:19:15 -0600

    events panel fixes

--- src/components/Collapsible.vue
+++ src/components/Collapsible.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <b-btn variant="primary" v-b-toggle="`${headertext.replace(/[\s']/ig, '-').toLowerCase()}-collapsible`">
+    <b-btn variant="primary" v-b-toggle="`collapsible-${this.cid}`">
       {{headertext}} <i class="fas fa-chevron-down"></i>
     </b-btn>
-    <b-collapse :id="`${headertext.replace(/[\s']/ig, '-').toLowerCase()}-collapsible`" @hidden="reflow()" @shown="reflow()">
+    <b-collapse :id="`collapsible-${this.cid}`" @hidden="reflow()" @shown="reflow()">
       <b-card>
         <slot></slot>
       </b-card>
@@ -16,12 +16,36 @@
   export default {
     name: 'Collapsible',
     props: ['headertext'],
+    data: function() {
+      return {
+        id: 0
+      }
+    },
     methods: {
       reflow: function() {
         // eslint-disable-next-line no-console
         console.log('triggered reflow');
         packeryEvents.$emit('layout', this.$refs.timerComponentGrid);
+      },
+      makeid: function() {
+        let text = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (let i = 0; i < 5; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
       }
     },
+    computed: {
+      cid: function() {
+        if (this.id) {
+          return id;
+        }
+        this.id = this.makeid();
+        return this.id;
+      }
+    }
   };
 </script>
--- src/components/panels/EventsPanel.vue
+++ src/components/panels/EventsPanel.vue
@@ -3,24 +3,31 @@
     <b-list-group>
       <b-list-group-item v-for="(event, index) in events" :key="event.id"
       :class="{ 'list-group-item-borderless': index !== events.length - 1, 'list-group-item-borderbottom': index === events.length - 1 }" v-if="event.active">
-        <h6 class="display-6 text-center">{{event.description}}</h6>
+        <h5 class="display-5 text-center">{{event.description}}</h5>
         <div class="text-center">{{event.tooltip}}</div><br>
-        <div class="text-center d-inline">
+        <div class="text-center bottom-pad">
           <b-badge variant="danger">{{event.victimNode}}</b-badge>
           <b-badge :variant="eventHealthVariant(event)">{{event.health}}% Remaining</b-badge>
-        </div><br>
-        <div class="text-center" v-if="event.rewards.length">
-          <b-badge tag="div" v-for="item in event.rewards" :key="item" variant="success">{{item}}</b-badge>
-        </div><br v-if="event.rewards.length">
-        <div class="text-center" v-for="(job, jIndex) in event.jobs" :key="`${job.type.replace(/\s/ig, '-').toLowerCase()}-${index}`">
-          <div>{{job.type}} <b-badge variant="info">{{job.enemyLevels[0]}}-{{job.enemyLevels[1]}}</b-badge></div>
-          <Collapsible headertext="Rewards">
-            <div><span v-for="s in job.standingStages" :key="`${s}-${jIndex}`">{{s}}</span><br></div>
-          </Collapsible>
-          <Collapsible headertext="Standing Stages">
-            <span><div v-for="r in job.rewardPool" :key="`${r}-${jIndex}`">{{r}}</div><br></span>
-          </Collapsible>
-        </div><br v-if="event.rewards.length">
+        </div>
+        <div class="text-center bottom-pad" v-for="reward in event.rewards" :key="makeid()">
+          <b-badge v-for="item in reward.items" :key="makeid()" variant="success">{{item}}</b-badge>
+          <b-badge v-for="item in reward.countedItems" :key="makeid()" variant="success">{{item}}</b-badge>
+          <b-badge v-if="reward.credits" variant="info">{{reward.credits}}cr</b-badge>
+        </div>
+
+        <b-row>
+          <b-col md="6" v-for="(job, jIndex) in event.jobs" :key="`${job.type.replace(/\s/ig, '-').toLowerCase()}-${index}`" class="bottom-pad">
+            <div class="text-center">
+              <div class="bottom-pad">{{job.type}} <b-badge variant="info">{{job.enemyLevels[0]}}-{{job.enemyLevels[1]}}</b-badge></div>
+              <Collapsible headertext="Standing Stages" class="bottom-pad">>
+                <div v-for="s in job.standingStages" :key="`standing-${makeid()}`">{{s}}</div>
+              </Collapsible>
+              <Collapsible headertext="Rewards">
+                <div v-for="r in job.rewardPool" :key="`rewards-${makeid()}`">{{r}}</div>
+              </Collapsible>
+            </div>
+          </b-col>
+        </b-row>
       </b-list-group-item>
       <NoDataItem v-if="events.length === 0" :text="headertext" />
     </b-list-group>
@@ -51,6 +58,16 @@
           healthState = 'danger';
         }
         return healthState;
+      },
+      makeid: function() {
+        let text = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (let i = 0; i < 5; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
       }
     },
     components: {

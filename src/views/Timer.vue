287a7c9a714a5a228ed927cfdb4c078e0b26d52f
Tobiah <matej@voboril.org>
2019-02-12 15:00:32 -0600

    decrease the number of columns at medium to xxs size

--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -87,7 +87,7 @@ export default {
       breakpoint: 'md',
       cols: 2,
       breakpoints: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
-      colsAll: { lg: 4, md: 3, sm: 2, xs: 2, xxs: 2 },
+      colsAll: { lg: 4, md: 2, sm: 1, xs: 1, xxs: 1 },
       isDraggable: true,
       isResizable: true,
       lastUpdate: 0

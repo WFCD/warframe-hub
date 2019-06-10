d14a750d6df368abc2bc40f4f91f47a0fc5461b2
Tobiah <tobiah@protonmail.com>
2019-06-10 13:18:58 -0500

    investigate using now for staging apps

---
+++ now.js
@@ -0,0 +1,12 @@
+{
+  "version": 2,
+  "builds": [{
+    "src": "package.json",
+    "use": "@now/static-build"
+  }],
+  "github": {
+    "silent": true
+  },
+  "name": "warframe-hub",
+  "scope": "wfcd"
+}

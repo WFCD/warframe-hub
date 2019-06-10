0e6dc01371b6a1c49da66d48cb09ceea805aab6c
Bitti09 <Bitti09@users.noreply.github.com>
2019-06-10 11:23:05 +0200

    Cleanup repo  in preparation for nuxt

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const app = express();
-app.use(serveStatic(__dirname + '/dist'));
-var port = process.env.PORT || 3000;
-app.listen(port);

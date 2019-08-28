0152051c65d76580ef1b30f020b87fab7bdaf3e5
Athari <athari.p@gmail.com>
2019-08-28 07:04:10 +0300

    Fixed Night Compact theme (#350)
    * Fixed margin in reset panel header
    
    * Removed unnecessary classes from bounty panel

--- src/components/panels/BountyPanel.vue
+++ src/components/panels/BountyPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <HubPanelWrap :title="headertext" class="bounties" :class="[syndicate]">
+  <HubPanelWrap :title="headertext" class="bounties">
     <b-list-group>
       <b-list-group-item v-if="syndicate && syndicate.active" class="list-group-item-borderless">
         <span class="pull-left">Bounties expire in:</span>

0e438bd654bef73e6c383200634d46c633761b52
Eric Lin <eric.lin@nyu.edu>
2019-11-05 18:06:31 -0500

    Lining that up was a pain

--- src/components/NoDataItem.vue
+++ src/components/NoDataItem.vue
@@ -1,14 +1,14 @@
 <template>
-  <b-list-group-item class="list-group-item-borderbottom">
+  <b-list-group-item class="list-group-item-borderbottom py-1">
     <span class="dim">
       <HubImg
         :src="loading"
         name="No Data"
-        width="50px"
-        height="50px"
+        width="40px"
+        height="40px"
         style="filter: invert(80%); padding-right:10px;"
       />
-      <div class="no-content-warning align-middle">No {{ text }} Present</div>
+      <div class="no-content-warning align-middle" style="margin-bottom:2px;">No {{ text }} Present</div>
     </span>
   </b-list-group-item>
 </template>
--- src/components/panels/SortiePanel.vue
+++ src/components/panels/SortiePanel.vue
@@ -22,7 +22,7 @@
       >
         <div class="indent-1">
           <span class="pull-left" style="padding-right: 10px">
-            <b>{{index + 1}}: {{ mission.missionType }} - {{ mission.node }}</b>
+            <b>{{ index + 1 }}: {{ mission.missionType }} - {{ mission.node }}</b>
           </span>
           <span v-b-tooltip.top :title="mission.modifierDescription" class="pull-left">
             {{ mission.modifier }}

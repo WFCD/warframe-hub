6a6e921a46d0c10c183a03014521c8d0cfb80d4d
Matt Voboril <tobiah@protonmail.com>
2020-03-05 12:27:15 -0600

    Image replacement (#409)

--- src/components/panels/DarvoDealsPanel.vue
+++ src/components/panels/DarvoDealsPanel.vue
@@ -41,7 +41,7 @@ import TimeBadge from '@/components/TimeBadge.vue';
 import HubPanelWrap from '@/components/HubPanelWrap';
 import NoDataItem from '@/components/NoDataItem.vue';
 
-import platinum from '@/assets/img/general/plat.png';
+import platinum from '@/assets/img/general/plat.webp';
 export default {
   name: 'DarvoDealsPanel',
   props: ['deals'],

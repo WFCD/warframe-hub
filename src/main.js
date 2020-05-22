37dede92db2a8964b4bc203a910c58e92bb1d9b3
Eric Lin <eric.lin@nyu.edu>
2020-05-21 21:44:27 -0400

    I'm being an idiot again

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 
 /* Sentry Reporting */
-/*
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
@@ -19,7 +18,7 @@ if (process.env.VUE_APP_DSN) {
     integrations: [new Integrations.Vue({ Vue, attachProps: true })],
   });
 }
-*/
+
 import VueMobileDetection from 'vue-mobile-detection';
 Vue.use(VueMobileDetection);
 

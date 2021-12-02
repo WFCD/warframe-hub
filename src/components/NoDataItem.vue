8512da156dbf537cb41486f2ce044c0e36c94140
Tobiah <tobiah@pm.me>
2021-12-01 23:04:58 -0600

    chore: add this invocation where it seemed right

--- src/components/NoDataItem.vue
+++ src/components/NoDataItem.vue
@@ -9,7 +9,7 @@
         style="filter: invert(80%); padding-right: 10px"
       />
       <div class="no-content-warning align-middle" style="margin-bottom: 2px">
-        {{ $t('nav.nodatatxt', { text }) }}
+        {{ this.$t('nav.nodatatxt', { text }) }}
       </div>
     </span>
   </b-list-group-item>

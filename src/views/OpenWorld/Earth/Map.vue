546f83a255aa01f15117149b676efffdde3c143e
Matt Voboril <tobiah@pm.me>
2021-12-16 13:20:48 -0600

    fix: map getters & wait a tick for params (#659)

--- src/views/OpenWorld/Earth/Map.vue
+++ src/views/OpenWorld/Earth/Map.vue
@@ -15,7 +15,6 @@ import Vue from 'vue';
 import L from 'leaflet';
 
 /* map stuff */
-
 import labels from '@/assets/json/geo/plains/labels.json';
 import fish from '@/assets/json/geo/plains/fishing.json';
 import grineer from '@/assets/json/geo/plains/grineer.json';
@@ -27,6 +26,7 @@ import cave from '@/assets/json/geo/plains/cave.json';
 import MapPopup from '@/components/MapPopup.vue';
 import OddityPopup from '@/components/OddityPopup.vue';
 import { cdn } from '@/utilities';
+import { mapGetters } from 'vuex';
 
 const plains = cdn('webp/maps/plains.webp');
 const caveIcon = cdn('webp/map_icons/normal-cave.webp');
@@ -39,22 +39,18 @@ const grineerMarker = L.icon({
   iconUrl: grineerIcon,
   iconSize: [25, 25],
 });
-
 const oddityMarker = L.icon({
   iconUrl: oddityIcon,
   iconSize: [33, 25],
 });
-
 const wispMarker = L.icon({
   iconUrl: wispIcon,
   iconSize: [50, 33],
 });
-
 const lureMarker = L.icon({
   iconUrl: lureIcon,
   iconSize: [50, 50],
 });
-
 const caveMarker = L.icon({
   iconUrl: caveIcon,
   iconSize: [25, 25],
@@ -84,145 +80,145 @@ function onEachOddity(feature, layer) {
   layer.bindPopup(popup.$mount().$el, { minWidth: 320 });
 }
 
-function data() {
-  return {
-    zoom: 0,
-    center: L.latLng(472, 535),
-    url: plains,
-    bounds: [
-      [0, 0],
-      [994, 1012],
-    ],
-    currentZoom: 11.5,
-    currentCenter: L.latLng(472, 535),
-    mapOptions: {
-      zoomSnap: 0.5,
-      attributionControl: false,
-    },
-    crs: L.CRS.Simple,
-    mapStyle: {
-      height: 'calc(100vh - 100px)',
-      width: '100%',
-    },
-    geo: [
-      {
-        name: 'Map Label',
-        json: labels,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return labelAlias(latlng)
-              .setStyle({
-                stroke: false,
-                fill: false,
-              })
-              .bindTooltip(feature.properties.name, {
-                permanent: true,
-                direction: 'center',
-                className: 'map-label',
-              })
-              .openTooltip();
-          },
-        },
-      },
-      {
-        name: 'Fishing',
-        json: fish,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng);
-          },
-          onEachFeature: onEachFeature,
-        },
-      },
-      {
-        name: 'Grineer Camp',
-        json: grineer,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng, { icon: grineerMarker });
-          },
-          onEachFeature: onEachFeature,
-        },
-      },
-      {
-        name: 'Oddity',
-        json: lorefish,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng, { icon: oddityMarker });
-          },
-          onEachFeature: onEachOddity,
-        },
-      },
-      {
-        name: 'Cetus Wisp',
-        json: wisp,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng, { icon: wispMarker });
-          },
-          onEachFeature: onEachFeature,
-        },
-      },
-      {
-        name: 'Vomvalyst Lure',
-        json: lure,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng, { icon: lureMarker });
-          },
-          onEachFeature: onEachFeature,
-        },
-      },
-      {
-        name: 'Cave Entrance',
-        json: cave,
-        opts: {
-          pointToLayer: function (feature, latlng) {
-            return markerAlias(latlng, { icon: caveMarker });
-          },
-          onEachFeature: onEachFeature,
-        },
-      },
-    ],
-  };
-}
-
-function mounted() {
-  this.map = this.$refs.pmap.mapObject;
-  // Get our toggle values from local storage
-  let toggles = this.$store.getters.poeMapToggles;
-  // Now add each of our geos to a new layer
-  var layerGroups = {};
-  data().geo.forEach((g) => {
-    var lg = L.layerGroup();
-    L.geoJSON(g.json, g.opts).addTo(lg);
-    layerGroups[g.name] = lg;
-    // and if that layer's toggle vlaue is true, add it to the map immediately
-    if (toggles[g.name + '-toggle-value']) {
-      lg.addTo(this.map);
-    }
-  });
-  // Add all of the layer groups to the map
-  L.control.layers(null, layerGroups, { collapsed: false }).addTo(this.map);
-  // Now wire up an event when the user toggles one of the layers to update localstorage
-  this.map.on('overlayadd overlayremove', (e) => {
-    toggles[e.name + '-toggle-value'] = e.type === 'overlayadd';
-    this.$store.commit('poeMapToggles', [toggles]);
-  });
-}
-
 const markerAlias = L.marker;
 const labelAlias = L.circleMarker;
 
 export default {
   name: 'cetus-map-view',
-  data: data,
+  data: function () {
+    return {
+      zoom: 0,
+      center: L.latLng(472, 535),
+      url: plains,
+      bounds: [
+        [0, 0],
+        [994, 1012],
+      ],
+      currentZoom: 11.5,
+      currentCenter: L.latLng(472, 535),
+      mapOptions: {
+        zoomSnap: 0.5,
+        attributionControl: false,
+      },
+      crs: L.CRS.Simple,
+      mapStyle: {
+        height: 'calc(100vh - 100px)',
+        width: '100%',
+      },
+      geo: [
+        {
+          name: 'Map Label',
+          json: labels,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return labelAlias(latlng)
+                .setStyle({
+                  stroke: false,
+                  fill: false,
+                })
+                .bindTooltip(feature.properties.name, {
+                  permanent: true,
+                  direction: 'center',
+                  className: 'map-label',
+                })
+                .openTooltip();
+            },
+          },
+        },
+        {
+          name: 'Fishing',
+          json: fish,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng);
+            },
+            onEachFeature: onEachFeature,
+          },
+        },
+        {
+          name: 'Grineer Camp',
+          json: grineer,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng, { icon: grineerMarker });
+            },
+            onEachFeature: onEachFeature,
+          },
+        },
+        {
+          name: 'Oddity',
+          json: lorefish,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng, { icon: oddityMarker });
+            },
+            onEachFeature: onEachOddity,
+          },
+        },
+        {
+          name: 'Cetus Wisp',
+          json: wisp,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng, { icon: wispMarker });
+            },
+            onEachFeature: onEachFeature,
+          },
+        },
+        {
+          name: 'Vomvalyst Lure',
+          json: lure,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng, { icon: lureMarker });
+            },
+            onEachFeature: onEachFeature,
+          },
+        },
+        {
+          name: 'Cave Entrance',
+          json: cave,
+          opts: {
+            pointToLayer: function (feature, latlng) {
+              return markerAlias(latlng, { icon: caveMarker });
+            },
+            onEachFeature: onEachFeature,
+          },
+        },
+      ],
+    };
+  },
   methods: {
     track() {
       this.$ga.page('/poe/map');
     },
   },
-  mounted: mounted,
+  computed: {
+    ...mapGetters('worldstate', { toggles: 'poeMapToggles' }),
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.map = this.$refs.pmap.mapObject;
+      // Get our toggle values from local storage
+      // Now add each of our geos to a new layer
+      const layerGroups = {};
+      this.geo.forEach((g) => {
+        const lg = L.layerGroup();
+        L.geoJSON(g.json, g.opts).addTo(lg);
+        layerGroups[g.name] = lg;
+        // and if that layer's toggle vlaue is true, add it to the map immediately
+        if (this.toggles[g.name + '-toggle-value']) {
+          lg.addTo(this.map);
+        }
+      });
+      // Add all of the layer groups to the map
+      L.control.layers(null, layerGroups, { collapsed: false }).addTo(this.map);
+      // Now wire up an event when the user toggles one of the layers to update localstorage
+      this.map.on('overlayadd overlayremove', (e) => {
+        this.toggles[e.name + '-toggle-value'] = e.type === 'overlayadd';
+        this.$store.commit('worldstate/poeMapToggles', [this.toggles]);
+      });
+    });
+  },
 };
 </script>

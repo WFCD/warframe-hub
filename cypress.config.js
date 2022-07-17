37472812535b222da12b19611baf53406120e4d0
Tobiah <tobiah@pm.me>
2022-07-17 00:06:45 -0500

    lint

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
     specPattern: 'cypress/e2e/**/*.spec.js',
   },
@@ -13,4 +13,4 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {},
     specPattern: 'components/**/*.spec.js',
   },
-})
+});

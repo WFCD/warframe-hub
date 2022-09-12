b3107048b1d67f5e46c59183584596750a351431
TobiTenno <tobiah@pm.me>
2022-09-11 19:12:58 -0500

    feat: support steel path fissure filtering

--- cypress/e2e/timers/load.spec.js
+++ cypress/e2e/timers/load.spec.js
@@ -4,9 +4,80 @@ describe('Timers', () => {
     cy.wait(1000);
   });
   describe('Fissures', () => {
+    let fissures;
     it('should load', () => {
-      const fissures = cy.get('div.fissures');
-      fissures.children().should('have.length', 2);
+      cy.get('div.fissures').as('fissures');
+      cy.get('@fissures').children().should('have.length', 2);
+
+      cy.get('div.fissures .list-group-item.list-group-item-borderbottom').as('fissureSelection');
+      cy.get('@fissureSelection').find('.btn.btn-outline-primary.btn-sm').as('selectionButtons');
+
+      cy.get('@selectionButtons').should('have.length', 4);
+      cy.get('@selectionButtons').eq(0).should('have.text', 'Fissures');
+      cy.get('@selectionButtons').eq(1).should('have.text', 'Void Storm');
+      cy.get('@selectionButtons').eq(2).should('have.text', 'Steel Path');
+      cy.get('@selectionButtons').eq(3).should('have.text', 'All');
+
+      cy.get('div.fissures>.list-group>.list-group-item.list-group-item-borderless.pb-0').then(($el) => {
+        // eslint-disable-next-line no-undef
+        fissures = Cypress.dom.wrap($el);
+        cy.get('@fissures')
+          .find('.list-group .list-group-item')
+          .should('have.length', fissures.length + 2);
+      });
+    });
+    it('should filter Steel Path', () => {
+      cy.get('div.fissures .list-group .list-group-item').as('fissures');
+      cy.get('div.fissures .list-group-item.list-group-item-borderbottom').as('fissureSelection');
+      cy.get('@fissureSelection').find('.btn.btn-outline-primary.btn-sm').as('selectionButtons');
+      cy.get('@selectionButtons').eq(2).click();
+
+      cy.get('div.fissures>.list-group>.list-group-item.list-group-item-borderless.pb-0').then(($el) => {
+        // eslint-disable-next-line no-undef
+        const steelFissures = Cypress.dom.wrap($el);
+        cy.get('@fissures')
+          .should('have.length', steelFissures.length + 2)
+          .and('have.length.lessThan', fissures.length);
+      });
+    });
+    it('should filter Void Storms', () => {
+      cy.get('div.fissures .list-group .list-group-item').as('fissures');
+      cy.get('div.fissures .list-group-item.list-group-item-borderbottom').as('fissureSelection');
+      cy.get('@fissureSelection').find('.btn.btn-outline-primary.btn-sm').as('selectionButtons');
+      cy.get('@selectionButtons').eq(2).click();
+
+      cy.get('div.fissures>.list-group>.list-group-item.list-group-item-borderless.pb-0').then(($el) => {
+        // eslint-disable-next-line no-undef
+        const stormFissures = Cypress.dom.wrap($el);
+        cy.get('@fissures')
+          .should('have.length', stormFissures.length + 2)
+          .and('have.length.lessThan', fissures.length);
+      });
+    });
+    it('should show icons for specialty types', () => {
+      cy.get('div.fissures .list-group .list-group-item.pb-0').as('fissures');
+      cy.get('div.fissures .list-group-item.list-group-item-borderbottom').as('fissureSelection');
+      cy.get('@fissureSelection').find('.btn.btn-outline-primary.btn-sm').as('selectionButtons');
+
+      // select Void Fissures
+      cy.get('@selectionButtons').eq(1).click();
+      cy.get('@fissures').each(($fissureRow) => {
+        cy.wrap($fissureRow)
+          .find('.li-mission-decorator.li-mission-decorator-lg')
+          .each(($img, idx) => {
+            if (idx === 1) cy.wrap($img).should('have.attr', 'title', 'Void Storm');
+          });
+      });
+
+      // select Steel Path
+      cy.get('@selectionButtons').eq(2).click();
+      cy.get('@fissures').each(($fissureRow) => {
+        cy.wrap($fissureRow)
+          .find('.li-mission-decorator.li-mission-decorator-lg')
+          .each(($img, idx) => {
+            if (idx === 1) cy.wrap($img).should('have.attr', 'title', 'Steel Path');
+          });
+      });
     });
   });
   describe('Nightwave', () => {

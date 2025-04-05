9bd7ee0a26b99da81c43d642d75bc0c146caebf6
HKayn <git@hkayn.com>
2025-04-05 13:29:54 +0200

    refactor: exchange `vue-notification` for `@kyvg/vue3-notification`

--- plugins/notifications.js
+++ plugins/notifications.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 /* Native notifications */
 import VueNativeNotification from 'vue-native-notification';
-import Notifications from 'vue-notification';
+import Vue3Notifications from '@kyvg/vue3-notification';
 
 Vue.use(VueNativeNotification, { requestOnNotify: true });
-Vue.use(Notifications);
+Vue.use(Vue3Notifications);

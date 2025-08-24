a30d3feddb3c4a5839062b8d5d56bcb803d39235
HKayn <git@hkayn.com>
2025-08-24 22:32:09 +0200

    fix: apply Vue notification plugins to `vueApp`

--- plugins/notifications.js
+++ plugins/notifications.js
@@ -3,7 +3,7 @@ import VueNativeNotification from 'vue-native-notification';
 import Vue3Notifications from '@kyvg/vue3-notification';
 import { defineNuxtPlugin } from '#app';
 
-export default defineNuxtPlugin((app) => {
-  app.use(VueNativeNotification, { requestOnNotify: true });
-  app.use(Vue3Notifications);
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueNativeNotification, { requestOnNotify: true });
+  nuxtApp.vueApp.use(Vue3Notifications);
 });
